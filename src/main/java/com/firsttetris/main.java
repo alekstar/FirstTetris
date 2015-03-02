96a938a7b8999886094ef9c6cbb84a2a40fd694e
alekstar <inet.tarasenko@gmail.com>
2015-03-02 16:31:29 +0200

    added class FieldPrinter for debugging

--- src/main/java/com/firsttetris/main.java
+++ src/main/java/com/firsttetris/main.java
@@ -1,10 +1,18 @@
 package com.firsttetris;
 
+import com.firsttetris.debug.FieldPrinter;
+import com.firsttetris.logic.Field;
+
 public class main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Field field = Field.create(10, 20);
+        FieldPrinter fieldPrinter = FieldPrinter.create(field);
+        field.setCellValue(3, 4, true);
+        field.setCellValue(3, 5, true);
+        field.setCellValue(3, 6, true);
+        field.setCellValue(4, 6, true);
+        fieldPrinter.print();
     }
 
 }
