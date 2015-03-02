64a022b436152b31db459e4b52e219d1751be30f
alekstar <inet.tarasenko@gmail.com>
2015-03-02 12:59:06 +0200

    initial commit

---
+++ src/main/java/com/firsttetris/exception/ArgumentIsNullException.java
@@ -0,0 +1,14 @@
+package com.firsttetris.exception;
+
+public class ArgumentIsNullException extends IllegalArgumentException {
+    private static final long serialVersionUID = 1L;
+
+    public ArgumentIsNullException(String argumentName) {
+        /*
+         * had to make this crappy code because constructor of superclass have
+         * to be called first
+         */
+        super((new StringBuilder()).append("Argument \"").append(argumentName)
+                .append("\" is null.").toString());
+    }
+}
---
+++ src/main/java/com/firsttetris/logic/Field.java
@@ -0,0 +1,84 @@
+package com.firsttetris.logic;
+
+import java.util.ArrayList;
+
+import com.firsttetris.exception.ArgumentIsNullException;
+
+public class Field {
+    private int width;
+    private int height;
+    private ArrayList<Boolean> field;
+    
+    private Field(int width, int height) {
+        setWidth(width);
+        setHeight(height);
+        setField(new ArrayList<Boolean>(calculateFieldArraySize()));
+    }
+
+    private int calculateFieldArraySize() {
+        return getWidth() * getHeight();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    private void setWidth(int width) {
+        if(width <= 0) {
+            throw new IllegalArgumentException("width: " + width);
+        }
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    private void setHeight(int height) {
+        if(height <= 0) {
+            throw new IllegalArgumentException("height: " + height);
+        }
+        this.height = height;
+    }
+
+    private ArrayList<Boolean> getField() {
+        return field;
+    }
+
+    private void setField(ArrayList<Boolean> field) {
+        if(field == null) {
+            throw new ArgumentIsNullException("field");
+        }
+        this.field = field;
+    }
+    
+    public boolean getCellValue(int xIndex, int yIndex) {
+        checkXIndex(xIndex);
+        checkYIndex(yIndex);
+        int arrayIndex = calculateArrayIndex(xIndex, yIndex);
+        return getField().get(arrayIndex).booleanValue();
+    }
+
+    private void checkYIndex(int yIndex) {
+        if((yIndex >= getHeight()) || (yIndex < 0)) {
+            throw new IllegalArgumentException("yIndex: " + yIndex);
+        }
+    }
+
+    private void checkXIndex(int xIndex) {
+        if((xIndex >= getWidth()) || (xIndex < 0)) {
+            throw new IllegalArgumentException("xIndex: " + xIndex);
+        }
+    }
+
+    private int calculateArrayIndex(int xIndex, int yIndex) {
+        return getWidth() * yIndex + xIndex;
+    }
+    
+    private void setCellValue(int xIndex, int yIndex, boolean value) {
+        checkXIndex(xIndex);
+        checkYIndex(yIndex);
+        int arrayIndex = calculateArrayIndex(xIndex, yIndex);
+        getField().set(arrayIndex, value);
+    }
+}
