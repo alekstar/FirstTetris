c07184604c913dc90bd18b09609a11ea4699f36f
alekstar <inet.tarasenko@gmail.com>
2015-03-02 13:46:56 +0200

    added test shouldFieldHaveHeight4 for Field class

--- src/main/test/com/firsttetris/logic/FieldTest.java
+++ src/main/test/com/firsttetris/logic/FieldTest.java
@@ -13,4 +13,11 @@ public class FieldTest {
         assertEquals(3, field.getWidth());
     }
     
+    @Test
+    public void shouldFieldHaveHeight4() {
+        Field field = null;
+        field = Field.create(3, 4);
+        assertEquals(4, field.getHeight());
+    }
+
 }
