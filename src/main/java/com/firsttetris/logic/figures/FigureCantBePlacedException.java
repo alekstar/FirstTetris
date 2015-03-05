32a45ead43df6ae4ca468f538edbe5a5a486ced2
alekstar <inet.tarasenko@gmail.com>
2015-03-05 15:46:08 +0200

    added serialVersionUID field to class FigureCantBePlacedException

--- src/main/java/com/firsttetris/logic/figures/FigureCantBePlacedException.java
+++ src/main/java/com/firsttetris/logic/figures/FigureCantBePlacedException.java
@@ -1,6 +1,7 @@
 package com.firsttetris.logic.figures;
 
 public class FigureCantBePlacedException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 
     public FigureCantBePlacedException() {
         super();
