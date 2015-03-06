78bb3d8e6efc82798c69ca7517a5570315d9ab2b
alekstar <inet.tarasenko@gmail.com>
2015-03-06 16:10:08 +0200

    added test shouldCreateCoordinatesWithXEquals15

--- src/main/test/com/firsttetris/logic/TetrisCoordinatesTest.java
+++ src/main/test/com/firsttetris/logic/TetrisCoordinatesTest.java
@@ -12,5 +12,12 @@ public class TetrisCoordinatesTest {
         coordinates = TetrisCoordinates.create(12, 15);
         assertEquals(12, coordinates.getX());
     }
+    
+    @Test
+    public void shouldCreateCoordinatesWithXEquals15() {
+        TetrisCoordinates coordinates = null;
+        coordinates = TetrisCoordinates.create(12, 15);
+        assertEquals(15, coordinates.getY());
+    }
 
 }
